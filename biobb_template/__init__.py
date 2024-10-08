fdff966dabce33eec56e5ddf86047b7368b27257
Rubén Chaves <ruben.chaves@irbbarcelona.org>
2024-10-08 12:26:55 +0200

    git init

---
+++ biobb_template/__init__.py
@@ -0,0 +1,3 @@
+name = "biobb_template"
+__all__ = ["template"]
+__version__ = "4.1.0"
---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="biobb_template",
+    version="4.1.0",
+    author="Biobb developers",
+    author_email="your@email.com",
+    description="Biobb_template is a complete code template to promote and facilitate the creation of new Biobbs by the community.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="Bioinformatics Workflows BioExcel Compatibility",
+    url="https://github.com/bioexcel/biobb_template",
+    project_urls={
+        "Documentation": "http://biobb-template.readthedocs.io/en/latest/",
+        "Bioexcel": "https://bioexcel.eu/"
+    },
+    packages=setuptools.find_packages(exclude=['adapters', 'docs', 'test']),
+    package_data={'biobb_template': ['py.typed']},
+    install_requires=['biobb_common==4.1.0'],
+    python_requires='>=3.8',
+    entry_points={
+        "console_scripts": [
+            "template = biobb_template.template.template:main"
+        ]
+    },
+    classifiers=(
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: Unix"
+    ),
+)
