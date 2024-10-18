a901663265614922d8d4b337e21c5c19f32ec404
Rubén Chaves <ruben.chaves@irbbarcelona.org>
2024-10-18 12:52:36 +0200

    Density tests

--- biobb_mem/test/unitests/ambertools/cpptraj_density.py
+++ biobb_mem/test/unitests/ambertools/cpptraj_density.py
@@ -2,12 +2,40 @@ from biobb_common.tools import test_fixtures as fx
 from biobb_mem.ambertools.cpptraj_density import cpptraj_density
 
 
-class TestCpptrajDensity():
+class TestCpptrajDensityDefault():
     def setup_class(self):
-        fx.test_setup(self, 'cpptraj_density')
+        fx.test_setup(self, 'cpptraj_density_default')
 
     def teardown_class(self):
-        #fx.test_teardown(self)
+        fx.test_teardown(self)
+        pass
+
+    def test_cpptraj_density(self):
+        returncode = cpptraj_density(properties=self.properties, **self.paths)
+        assert fx.not_empty(self.paths['output_cpptraj_path'])
+        assert fx.equal(self.paths['output_cpptraj_path'], self.paths['ref_output_cpptraj_path'])
+        assert fx.exe_success(returncode)
+
+class TestCpptrajDensityMask():
+    def setup_class(self):
+        fx.test_setup(self, 'cpptraj_density_mask')
+
+    def teardown_class(self):
+        fx.test_teardown(self)
+        pass
+
+    def test_cpptraj_density(self):
+        returncode = cpptraj_density(properties=self.properties, **self.paths)
+        assert fx.not_empty(self.paths['output_cpptraj_path'])
+        assert fx.equal(self.paths['output_cpptraj_path'], self.paths['ref_output_cpptraj_path'])
+        assert fx.exe_success(returncode)
+
+class TestCpptrajDensityMask():
+    def setup_class(self):
+        fx.test_setup(self, 'cpptraj_density_complex')
+
+    def teardown_class(self):
+        fx.test_teardown(self)
         pass
 
     def test_cpptraj_density(self):
